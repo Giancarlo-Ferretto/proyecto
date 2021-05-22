c0fb80a37141f08d16c49e50fa14d7b3ac55986d
gferretto <54486248+combatdev@users.noreply.github.com>
2021-05-22 12:52:38 -0400

    CRUD productos

--- backend/src/controllers/producto.controller.ts
+++ backend/src/controllers/producto.controller.ts
@@ -1,17 +1,46 @@
 import connection from '../database';
 import { producto } from '../models/producto';
 
-export function getProductos(req:any, res:any): Promise<Response> {
+//CRUD = Create (POST) - Read (GET) - Update (POST) - Delete (delete)
+//GET
+export function getProductos(req:any, res:any) {
     return connection.query("SELECT * FROM productos", (req_:any, results:any) => {
         res.status(200).send(results);
     });
 }
 
+export function getProducto(req:any, res:any) {
+    let id = req.params.id;
+    return connection.query("SELECT * FROM productos WHERE ID = ?", [id], (req_:any, results:any) => {
+        res.status(200).send(results);
+    });
+}
+
+//POST
 export function postProducto(req:any, res:any) {
     const newProducto:producto = JSON.parse(JSON.stringify(req.body));
 
     return connection.query("INSERT INTO productos SET ?", [newProducto], function (error:any, results:any, fields:any) {
         if (error) throw error;
         res.status(201).send(`Producto insertado con el id:${results.insertId}!`);
+    });
+}
+
+//PUT
+export function putProducto(req:any, res:any) {
+    let id = req.params.id;
+    const updatedProducto:producto = JSON.parse(JSON.stringify(req.body));
+
+    return connection.query("UPDATE productos SET ? WHERE ID = ?", [updatedProducto, id], (req_:any, results:any) => {
+        res.status(200).send(results);
+    });
+}
+
+//DELETE
+export function deleteProducto(req:any, res:any) {
+    let id = req.params.id;
+    
+    return connection.query("DELETE FROM productos WHERE ID = ?", [id], (req_:any, results:any) => {
+        res.status(200).send(`Producto eliminado!`);
     });
 }
--- backend/src/routes/producto.routes.ts
+++ backend/src/routes/producto.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 const router = Router();
 
-import { getProductos, postProducto } from '../controllers/producto.controller'
+import { getProductos, getProducto, postProducto, putProducto, deleteProducto } from '../controllers/producto.controller'
 
 router.route('/')
       .get(getProductos)
       .post(postProducto);
+
+router.route('/:id')
+      .get(getProducto)
+      .delete(deleteProducto)
+      .put(putProducto);
 
 export default router;
