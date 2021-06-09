55fb215753ec1534a8fb076b25743ecb7c53e99b
gferretto <54486248+combatdev@users.noreply.github.com>
2021-06-09 00:00:15 -0400

    auth.controller signIn

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -5,21 +5,46 @@ require('dotenv').config();
 import { User } from "../models/user";
 import * as passwordEncryptor from "../utils/passwordEncryptor"
 
-export function signIn(req:any, res:any) {
-    return res.send('sign in!');
+export const signIn = async (req:any, res:any) => {
+    try {
+        connection.query("SELECT id, email, password FROM users WHERE email = ? LIMIT 1", [req.body.email], async (error:any, results:any) => {
+            if (error) throw error;
+            if (!results.length) return res.status(400).json({message: "El correo electrónico no está registrado."});
+
+            const matchPassword = await passwordEncryptor.comparePassword(req.body.password, results[0].password);
+            if (matchPassword == false) return res.status(401).json({token: null, message:"La contraseña ingresada es incorrecta."});
+
+            if (process.env.API_KEY) {
+                const token = jwt.sign({id: results[0].id}, process.env.API_KEY, {
+                    expiresIn: 86400,
+                });
+                res.json({token});
+            }
+        });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
 }
 
-export const signUp = async(req:any, res:any) => {
-    const newUser:User = req.body;
-    newUser.password = await passwordEncryptor.encryptPassword(req.body.password);
+export const signUp = async (req:any, res:any) => {
+    try {
+        const newUser:User = req.body;
+        newUser.password = await passwordEncryptor.encryptPassword(req.body.password);
 
-    return connection.query("INSERT INTO users SET ?", [newUser], function (error:any, results:any, fields:any) {
-        if (error) throw error;
-        
-        if(process.env.API_KEY) {
-            const token = jwt.sign({id: results.id}, process.env.API_KEY);
-
-            res.status(201).json({token});
-        }
-    });
+        return connection.query("INSERT INTO users SET ?", [newUser], function (error:any, results:any, fields:any) {
+            if (error) throw error;
+            if (process.env.API_KEY) {
+                const token = jwt.sign({id: results.id}, process.env.API_KEY, {
+                    expiresIn: 86400,
+                });
+                res.status(201).json({token});
+            }
+        });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
 }
