e11d94079cb77ab693a649ad33ca1f30f3ccb0c2
gferretto <54486248+combatdev@users.noreply.github.com>
2021-06-09 10:42:15 -0400

    fix user token

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -10,12 +10,12 @@ export const signIn = async (req:any, res:any) => {
         connection.query("SELECT id, email, password FROM users WHERE email = ? LIMIT 1", [req.body.email], async (error:any, results:any) => {
             if (error) throw error;
             if (!results.length) return res.status(400).json({message: "El correo electrónico no está registrado."});
-
             const matchPassword = await passwordEncryptor.comparePassword(req.body.password, results[0].password);
             if (matchPassword == false) return res.status(401).json({token: null, message:"La contraseña ingresada es incorrecta."});
-
+            
+            const selectedUser:User = results[0];
             if (process.env.API_KEY) {
-                const token = jwt.sign({id: results[0].id}, process.env.API_KEY, {
+                const token = jwt.sign({id: selectedUser.id}, process.env.API_KEY, {
                     expiresIn: 86400,
                 });
                 res.json({token});
@@ -36,7 +36,8 @@ export const signUp = async (req:any, res:any) => {
         return connection.query("INSERT INTO users SET ?", [newUser], function (error:any, results:any) {
             if (error) throw error;
             if (process.env.API_KEY) {
-                const token = jwt.sign({id: results.id}, process.env.API_KEY, {
+                newUser.id = results.insertId;
+                const token = jwt.sign({id: newUser.id}, process.env.API_KEY, {
                     expiresIn: 86400,
                 });
                 res.status(201).json({token});
