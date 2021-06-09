24a1d3205f68716aa7a2cda9f49a9c788b40e140
gferretto <54486248+combatdev@users.noreply.github.com>
2021-06-09 10:42:47 -0400

    protected user routes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,10 +4,10 @@ const router = Router();
 import * as userController from '../controllers/user.controller'
 import * as checkToken from '../middlewares/checkToken';
 
-router.post('/', [checkToken.checkToken], userController.createUser);
-router.get('/', [checkToken.checkToken], userController.getUsers);
-router.get('/:id', [checkToken.checkToken], userController.getUserById);
-router.put('/:id', [checkToken.checkToken], userController.updateUserById);
-router.delete('/:id', [checkToken.checkToken], userController.deleteUserById);
+router.post('/', [checkToken.checkToken, checkToken.isAdmin], userController.createUser);
+router.get('/', [checkToken.checkToken, checkToken.isAdmin], userController.getUsers);
+router.get('/:id', [checkToken.checkToken, checkToken.isAdmin], userController.getUserById);
+router.put('/:id', [checkToken.checkToken, checkToken.isAdmin], userController.updateUserById);
+router.delete('/:id', [checkToken.checkToken, checkToken.isAdmin], userController.deleteUserById);
 
 export default router;
