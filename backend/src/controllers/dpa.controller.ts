0dc542896c11341147d4ac92274fbd9e6cb82576
gferretto <54486248+combatdev@users.noreply.github.com>
2021-06-11 21:40:00 -0400

    Consumo API división político administrativa

--- backend/src/controllers/dpa.controller.ts
+++ backend/src/controllers/dpa.controller.ts
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 export const getRegiones = async (req:any, res:any) => {
     await axios.get('https://apis.digital.gob.cl/dpa/regiones/')
-    .then(async (response:any) => {
+    .then((response:any) => {
         res.status(200).send(response.data);
     })
     .catch((error:any) => {
@@ -13,7 +13,7 @@ export const getRegiones = async (req:any, res:any) => {
 export const getComunas = async (req:any, res:any) => {
     const regionId = req.params.id;
     await axios.get('https://apis.digital.gob.cl/dpa/regiones/'+regionId+'/comunas')
-    .then(async (response:any) => {
+    .then((response:any) => {
         res.status(200).send(response.data);
     })
     .catch((error:any) => {
