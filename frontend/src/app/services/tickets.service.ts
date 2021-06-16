613cdb1b7181ce3d7699c7ccda7e4aa1e1c02dd7
gferretto <54486248+combatdev@users.noreply.github.com>
2021-06-15 20:39:19 -0400

    ticket service

---
+++ frontend/src/app/services/tickets.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Ticket } from '../interfaces/ticket';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TicketsService {
+
+  constructor(private http:HttpClient) { }
+
+  createTicket(ticket:Ticket) {
+    return this.http.post(`${environment.API_URL}tickets`, ticket);
+  }
+
+  getTickets() {
+    return this.http.get<Ticket[]>(`${environment.API_URL}tickets`);
+  }
+
+  getTicketById(ticketId:number) {
+    return this.http.get(`${environment.API_URL}tickets/${ticketId}`);
+  }
+
+  getTicketsByUserId(userId:number) {
+    return this.http.get<Ticket[]>(`${environment.API_URL}tickets/${userId}/user`);
+  }
+
+  updateTicketById(ticket:Ticket) {
+    return this.http.put(`${environment.API_URL}tickets/${ticket.ID}`, ticket);
+  }
+
+  deleteTicketById(ticketId:number) {
+    return this.http.delete(`${environment.API_URL}tickets/${ticketId}`);
+  }
+}
