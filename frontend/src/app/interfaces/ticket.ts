3ebba3fd11dc67ffca2ad51bdcac5ee19fce1430
gferretto <54486248+combatdev@users.noreply.github.com>
2021-06-15 20:38:50 -0400

    ticket interface

---
+++ frontend/src/app/interfaces/ticket.ts
@@ -0,0 +1,10 @@
+export interface Ticket {
+    ID:number;
+    userId:number;
+    status:string;
+    priority:string;
+    category:string;
+    subject:string;
+    description:string;
+    creationDate:string;
+}
