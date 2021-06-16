ab9ecf36f7e1401071b672b87474c05be059918c
gferretto <54486248+combatdev@users.noreply.github.com>
2021-06-16 10:49:05 -0400

    Update dpa.routes.ts

--- backend/src/routes/dpa.routes.ts
+++ backend/src/routes/dpa.routes.ts
@@ -2,9 +2,8 @@ import { Router } from 'express';
 const router = Router();
 
 import * as dpaController from '../controllers/dpa.controller';
-import * as checkToken from '../middlewares/checkToken';
 
-router.get('/', [checkToken.checkToken], dpaController.getRegiones);
-router.get('/:id/comunas', [checkToken.checkToken], dpaController.getComunas);
+router.get('/', dpaController.getRegiones);
+router.get('/:id/comunas', dpaController.getComunas);
 
 export default router;
