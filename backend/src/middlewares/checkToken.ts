5dfb33bf349e7ee0962f6f25501a2f55cc9f6908
gferretto <54486248+combatdev@users.noreply.github.com>
2021-06-16 10:16:08 -0400

    Update checkToken.ts

--- backend/src/middlewares/checkToken.ts
+++ backend/src/middlewares/checkToken.ts
@@ -4,18 +4,18 @@ require('dotenv').config();
 
 export const checkToken = async (req:any, res:any, next:any) => {
     const token = req.headers["access-token"];
-    if (!token) return res.status(403).json({message: "Unauthorized! Unspecified access-token."});
+    if (!token) return res.status(401).json({message: "¡No has especificado el token de acceso!"});
     try {
         if (process.env.API_KEY) {
             jwt.verify(token, process.env.API_KEY, (err:any, decoded:any) => {
-                if (err) return res.status(403).json({message: "Unauthorized! Invalid access-token."});
+                if (err) return res.status(401).json({message: "¡El token especificado es inválido o ha expirado!"});
                 req.userId = decoded.id;
                 next();
             });
         }
     }
     catch (error) {
-        return res.status(403).json({message: "Unauthorized!"});
+        return res.status(401).json({message: error});
     }
 }
 
@@ -23,8 +23,8 @@ export const isAdmin = async (req:any, res:any, next:any) => {
     try {
         connection.query("SELECT isAdmin FROM users WHERE ID = ?", [req.userId], async (error:any, results:any) => {
             if (error) throw error;
-            if (!results.length) return res.status(403).json({message: "No tienes permisos de administrador."});
-            if (!results[0].isAdmin) return res.status(403).json({message: "No tienes permisos de administrador."});
+            if (!results.length) return res.status(403).json({message: "¡No tienes permisos de administrador!"});
+            if (!results[0].isAdmin) return res.status(403).json({message: "¡No tienes permisos de administrador!"});
             next();
         });
     }
